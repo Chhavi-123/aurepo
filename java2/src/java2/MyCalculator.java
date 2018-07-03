4ebee751a737d5f55df499ede6d88629ab45ee24
chhavi <chhavi@CHSA-DEL-01>
2018-07-03 16:17:33 +0530

    initial commit

---
+++ java2/src/java2/MyCalculator.java
@@ -0,0 +1,8 @@
+package java2;
+
+public class MyCalculator {
+	public static void main(String[] args) {
+		addition a= new addition();
+		System.out.println("Sum is "+ a.sum(new int[] {1,2,3,4}));
+	}
+}
---
+++ java2/src/java2/addition.java
@@ -0,0 +1,11 @@
+package java2;
+
+public class addition {
+   public int sum(int[] a) {
+	   int sum1=0;
+	   for (int i:a) {
+		sum1+=i;
+     	}
+	   return sum1;
+   }
+}
